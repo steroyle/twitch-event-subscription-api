1a5e885bfb01cd2a8bdab34afc114301121cf7f9
steroyle <steroyle@gmail.com>
2022-01-24 15:50:43 +0000

    initial commit

---
+++ netlify/functions/twitch-subscription.js
@@ -0,0 +1,15 @@
+exports.handler = async (event) => {
+
+  console.log({ event })
+  const { headers = [] } = event
+
+  const type = headers['twitch-eventsub-message-type'] || 'no-type'
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      type,
+      message: 'ok'
+    }),
+  }
+}

b9afa52b06640d5fa47881fc5afc97555f72b341
steroyle <steroyle@gmail.com>
2022-01-25 11:15:38 +0000

    added sub verification

--- netlify/functions/twitch-subscription.js
+++ netlify/functions/twitch-subscription.js
@@ -1,17 +1,26 @@
+const VERIFICATION_TYPE = "webhook_callback_verification"
+const NOTIFICATION_TYPE = "notification"
+
 exports.handler = async (event) => {
 
-  console.log({ event })
-  const { headers = [] } = event
+  const body = JSON.parse(event.body)
+  const messageType = event.headers["twitch-eventsub-message-type"];
 
-  const type = headers['twitch-eventsub-message-type'] || 'no-type'
-  const subType = headers['twitch-eventsub-subscription-type'] || 'no-sub-type'
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      type,
-      subType,
-      message: 'ok'
-    }),
+  if (messageType === VERIFICATION_TYPE) {
+    return {
+      statusCode: 200,
+      body: body.challenge // IMPORTANT, challenge is a key in body
+    }
   }
+
+  else if (messageType === NOTIFICATION_TYPE) {
+    const event = body.event
+
+    console.log("Twitch activated this webhook! Here's the payload: ", event)
+    return {
+      statusCode: 200,
+      body: "ok"
+    }
+  }
+
 }
